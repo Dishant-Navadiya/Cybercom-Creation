03209ecb78a3b6150fd20e361470c932591c5511
Dishant Navadiya <dishantnavadiya585858@gmail.com>
2021-01-25 19:05:13 +0530

    added successfully

--- Exam/js/display-user-name.js
+++
@@ -1,9 +0,0 @@
-const currentUser = JSON.parse(sessionStorage.getItem("activeUser"));
-const userName = document.getElementById("userName");
-
-const printAcitveUserName = () => {
-  userName.innerHTML = `Hello [${currentUser.name}]`;
-};
-if (currentUser) {
-  printAcitveUserName();
-}
--- Exam/js/sub-user.js
+++
@@ -1,9 +0,0 @@
-const printHappy = document.getElementById("printHappy");
-
-const gettingActive = JSON.parse(sessionStorage.getItem("activeUser"));
-if (
-  new Date(gettingActive.birthdayDate).toDateString() ==
-  new Date().toDateString()
-) {
-  printHappy.innerText = "Happy Birthday";
-}
